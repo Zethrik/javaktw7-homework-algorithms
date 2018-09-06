7b1935016dc16668a9587307b733c926b8d70924
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2018-09-06 17:56:24 +0200

    PeselUtility and LongSum tests passed

--- src/main/java/pl/sdacademy/algorithms/LongSum.java
+++ src/main/java/pl/sdacademy/algorithms/LongSum.java
@@ -5,6 +5,13 @@ import java.math.BigInteger;
 public class LongSum {
 
     public String sum(Long[] longs) {
-        return "";
+        BigInteger bigInteger = new BigInteger("0");
+
+        for (Long aLong : longs) {
+            String value = aLong.toString();
+            bigInteger = bigInteger.add(new BigInteger(value));
+        }
+
+        return bigInteger.toString();
     }
 }
--- src/main/java/pl/sdacademy/algorithms/PeselUtility.java
+++ src/main/java/pl/sdacademy/algorithms/PeselUtility.java
@@ -2,9 +2,29 @@ package pl.sdacademy.algorithms;
 
 public class PeselUtility {
     public boolean isValid(String pesel) {
-        return false;
+        int sum = 0;
+
+        for (int i = 0; i < pesel.length(); i++) {
+            int digit = Character.getNumericValue(pesel.charAt(i));
+
+            if (i == 0 || i == 4 || i == 8 || i == 10) {
+                sum += digit;
+            } else if (i == 1 || i == 5 || i == 9) {
+                sum += digit * 3;
+            } else if (i == 2 || i == 6) {
+                sum += digit * 7;
+            } else if (i == 3 || i == 7) {
+                sum += digit * 9;
+            }
+        }
+
+        return sum % 10 == 0;
     }
+
+
     public boolean isMale(String pesel) {
-        return false;
+        int digit = Character.getNumericValue(pesel.charAt(9));
+
+        return digit % 2 != 0;
     }
 }
