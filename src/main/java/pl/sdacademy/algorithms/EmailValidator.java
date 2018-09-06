36e65f52b079824af61ad777fdf588365287105d
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2018-09-06 18:20:49 +0200

    EmailValidator tests passed

--- src/main/java/pl/sdacademy/algorithms/EmailValidator.java
+++ src/main/java/pl/sdacademy/algorithms/EmailValidator.java
@@ -3,6 +3,27 @@ package pl.sdacademy.algorithms;
 public class EmailValidator {
 
     public boolean isValid(String email) {
-        return false;
+        boolean result = true;
+
+        // checking the number of "ats"
+        int numberOfAts = 0;
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == '@') {
+                numberOfAts++;
+            }
+        }
+        if (numberOfAts != 1) {
+            result = false;
+        }
+
+        // checking the domain's length
+        String[] addressParts1 = email.split("@");
+        String domain = addressParts1[1];
+
+        if (domain.charAt(domain.length() - 2) == '.') {
+            result = false;
+        }
+
+        return result;
     }
 }
